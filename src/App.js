8d33fe510e92da0e3c97c89834a3dda9e41059fd
MissHalle <100787203+MissHalle@users.noreply.github.com>
2022-06-29 18:37:32 -0500

    added axios, fixed, css, using state in Weather.js for api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           React Weather Application created by Halle Gregory and
           <a
